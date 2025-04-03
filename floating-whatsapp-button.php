50e1b5b8b05f0f87a812d3ba8dc06b9d8b5021e1
Andrei de Souza <71538656+youngnix@users.noreply.github.com>
2025-04-03 10:45:53 -0300

    try to fix it

--- floating-whatsapp-button.php
+++ floating-whatsapp-button.php
@@ -12,6 +12,20 @@ Version: 1.0.0
 Author URI: 
 */
 
+function floating_whatsapp_button_activate() {
+	// pass
+}
+
+register_activation_hook(__FILE__, 'floating_whatsapp_button_activate');
+
+function load_plugin() {
+
+    if ( is_admin() && get_option( 'Activated_Plugin' ) == 'Plugin-Slug' ) {
+        delete_option( 'Activated_Plugin' );
+    }
+}
+add_action( 'admin_init', 'load_plugin' );
+
 add_action("wp_head", function() {
 	?>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
